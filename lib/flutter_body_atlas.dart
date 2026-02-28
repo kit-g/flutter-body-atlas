c32d300ce134f4629dfa2c8a0c8804c49389cbde
_kitg <gerasimovkit@gmail.com>
2026-02-28 14:53:08 -0500

    feat: MuscleGroup and Muscle in export list

--- lib/flutter_body_atlas.dart
+++ lib/flutter_body_atlas.dart
@@ -2,5 +2,13 @@ library;
 
 export 'src/assets.dart' show AtlasAsset;
 export 'src/models/contracts.dart';
-export 'src/models/muscle.dart' show MuscleInfo, MuscleCatalog, MuscleResolver, MuscleLocalizerEn, MuscleSearch;
+export 'src/models/muscle.dart'
+    show
+        MuscleInfo, //
+        MuscleCatalog,
+        MuscleResolver,
+        MuscleLocalizerEn,
+        MuscleSearch,
+        MuscleGroup,
+        Muscle;
 export 'src/widgets/widgets.dart' show BodyAtlasView;
